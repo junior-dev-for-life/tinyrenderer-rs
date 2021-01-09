f81746591e9e8ea8ddab9b8eefed01f5f5dbd96a
Renjith <grenjithr@gmail.com>
2021-01-08 18:47:41 -0600

    Change one pixel

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 
-fn main() {
- render();
+struct Pixel {
+    x: u32,
+    y: u32
 }
 
-fn render() {
+fn main() {
+ render(Pixel{ x: 0, y: 0 } );
+}
+
+
+
+fn render(pixel: Pixel) {
     let width = 800;
     let height = 800;
     let mut imgbuf = image::ImageBuffer::new(width, height);
 
-    for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
+    for (x, _, pixel) in imgbuf.enumerate_pixels_mut() {
         *pixel = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
     }
 
+    imgbuf.put_pixel(pixel.x, pixel.y, image::Rgb([255 as u8, 0 as u8, 0 as u8]));
     imgbuf.save("output.png").unwrap();
 }
