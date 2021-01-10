b976e9da8e3bdb7e070577e1d873093b072f58ab
Renjith <grenjithr@gmail.com>
2021-01-10 01:20:06 -0600

    Wireframe rendering worksgit add .

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,29 @@ struct Pixel {
     y: u32
 }
 
+const WIDTH: u32 = 500;
+const HEIGHT: u32 = 500;
+
 fn main() {
     let obj = obj_reader::read_obj("./obj-files/african-head.obj").unwrap();
 
     let mut points: Vec<Pixel> = vec![];
     let num_of_faces = obj.faces.len();
 
+    let width = (WIDTH - 1) as f32;
+    let height = (HEIGHT - 1) as f32;
+
     for i in 0..num_of_faces {
         let face = &obj.faces[i];
         for j in 0..3 {
             let v0 = &obj.verts[face[j] as usize];
             let k = (j + 1) % 3;
-            let v1 = &obj.verts[k];
+            let v1 = &obj.verts[face[k] as usize];
 
-            let x0 = ((v0[0] + 1.) * 99.0 / 2.) as i32;
-            let y0 = ((v0[1] + 1.) * 99.0 / 2.) as i32;
-            let x1 = ((v1[1] + 1.) * 99.0 / 2.) as i32;
-            let y1 = ((v1[1] + 1.) * 99.0 / 2.) as i32;
+            let x0 = ((v0[0] + 1.) * width / 2.) as i32;
+            let y0 = ((v0[1] + 1.) * height / 2.) as i32;
+            let x1 = ((v1[0] + 1.) * width / 2.) as i32;
+            let y1 = ((v1[1] + 1.) * height / 2.) as i32;
 
             let line = dda_algorithm(x0, y0, x1, y1);
             for pixel in line.iter() {
@@ -58,9 +64,7 @@ fn dda_algorithm(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Pixel> {
 
 #[allow(dead_code)]
 fn render(points: Vec<Pixel>) {
-    let width = 100;
-    let height = 100;
-    let mut imgbuf = image::ImageBuffer::new(width, height);
+    let mut imgbuf = image::ImageBuffer::new(WIDTH, HEIGHT);
 
     for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
         *pixel = image::Rgb([0 as u8, 0 as u8, 0 as u8]);
