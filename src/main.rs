3f1e2fbaa23351954c9929533a6cc1c2d9895a8e
Renjith <grenjithr@gmail.com>
2021-01-10 01:05:37 -0600

    Rendering obj file terribly now

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,29 @@ struct Pixel {
 fn main() {
     let obj = obj_reader::read_obj("./obj-files/african-head.obj").unwrap();
 
-    for i in obj.verts {
-        println!("{}", i);
+    let mut points: Vec<Pixel> = vec![];
+    let num_of_faces = obj.faces.len();
+
+    for i in 0..num_of_faces {
+        let face = &obj.faces[i];
+        for j in 0..3 {
+            let v0 = &obj.verts[face[j] as usize];
+            let k = (j + 1) % 3;
+            let v1 = &obj.verts[k];
+
+            let x0 = ((v0[0] + 1.) * 99.0 / 2.) as i32;
+            let y0 = ((v0[1] + 1.) * 99.0 / 2.) as i32;
+            let x1 = ((v1[1] + 1.) * 99.0 / 2.) as i32;
+            let y1 = ((v1[1] + 1.) * 99.0 / 2.) as i32;
+
+            let line = dda_algorithm(x0, y0, x1, y1);
+            for pixel in line.iter() {
+                points.push(pixel.clone());
+            }
+        }
     }
+
+    render(points);
 }
 
 #[allow(dead_code)]
--- src/obj_reader.rs
+++ src/obj_reader.rs
@@ -4,14 +4,22 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 pub struct Obj {
-    pub verts: Vec<f32>
+    pub verts: Vec<Vec<f32>>,
+    pub faces: Vec<Vec<i32>>
+}
+
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
 }
 
 pub fn read_obj(filename: &str) -> Result<Obj, io::Error> {
     let f = File::open(filename)?;
     let file = BufReader::new(&f);
 
-    let mut verts: Vec<f32> = vec![];
+    let mut verts: Vec<Vec<f32>> = vec![];
+    let mut faces: Vec<Vec<i32>> = vec![];
 
     for line in file.lines() {
         let str = &line.unwrap();
@@ -21,19 +29,40 @@ pub fn read_obj(filename: &str) -> Result<Obj, io::Error> {
 
             match &segments[..] {
                 [_, x, y, z, _] => {
-                    verts.push(x.parse::<f32>().unwrap());
-                    verts.push(y.parse::<f32>().unwrap());
-                    verts.push(z.parse::<f32>().unwrap());
+                    verts.push(vec![
+                        x.parse::<f32>().unwrap(),
+                        y.parse::<f32>().unwrap(),
+                        z.parse::<f32>().unwrap()
+                    ]);
                 },
                 [_, x, y, z] => {
-                    verts.push(x.parse::<f32>().unwrap());
-                    verts.push(y.parse::<f32>().unwrap());
-                    verts.push(z.parse::<f32>().unwrap());
+                    verts.push(vec![
+                        x.parse::<f32>().unwrap(),
+                        y.parse::<f32>().unwrap(),
+                        z.parse::<f32>().unwrap()
+                    ]);
                 },
                 _ => panic!("Wrong number of parameters")
+            }
+        } else if str.starts_with("f ") {
+            let segments: Vec<&str> = str.split(" ").collect();
+            match &segments[..] {
+                [_, x, y, z] => {
+                    let mut face: Vec<i32> = vec![];
+                    let first = x.split("/").collect::<Vec<&str>>();
+                    let second = y.split("/").collect::<Vec<&str>>();
+                    let third = z.split("/").collect::<Vec<&str>>();
+
+                    face.push(first[0].parse::<i32>().unwrap() - 1);
+                    face.push(second[0].parse::<i32>().unwrap() - 1);
+                    face.push(third[0].parse::<i32>().unwrap() - 1);
+
+                    faces.push(face);
+                },
+                _ => panic!("Wrong face line")
             }
         };
     }
 
-    Ok( Obj { verts } )
+    Ok( Obj { verts, faces } )
 }
