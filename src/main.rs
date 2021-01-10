7b1add9846a938e5f105c99f2e2a777b78a74d3e
Renjith <grenjithr@gmail.com>
2021-01-09 22:59:28 -0600

    Implement DDA algorithm

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,30 @@ struct Pixel {
 }
 
 fn main() {
-    let points = line(13 , 20, 80, 40);
-    render(points);
+    let dda_points = dda_algorithm(90 , 80, 60, 50);
+
+    render(dda_points);
 }
 
-fn line(x1: u32, y1: u32, x2: u32, y2: u32) -> Vec<Pixel> {
-    let mut output = vec![];
+fn dda_algorithm(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<Pixel> {
+    let x_dist = x2 - x1;
+    let y_dist = y2 - y1;
+    let steps = if x_dist.abs() > y_dist.abs() { x_dist.abs() } else { y_dist.abs() };
+    let x_increment = x_dist / steps;
+    let y_increment = y_dist / steps;
 
-    let mut i = 0.0;
+    let mut x = x1;
+    let mut y = y1;
+    let mut points: Vec<Pixel> = vec![];
+    points.push(Pixel { x: x as u32,  y: y as u32 });
 
-    while i <= 1.0 {
-        let x = (((x1 + (x2 - x1)) as f32) * i) as u32;
-        let y = (((y1 + (y2 - y1)) as f32) * i) as u32;
-        output.push(Pixel { x, y });
-        i = i + 0.01;
+    for i in 1..=steps {
+        x += x_increment;
+        y += y_increment;
+        points.push(Pixel{ x: x as u32, y: y as u32 });
     }
 
-    return output;
+    return points;
 }
 
 fn render(points: Vec<Pixel>) {
@@ -37,5 +44,5 @@ fn render(points: Vec<Pixel>) {
     }
 
     image::imageops::flip_vertical_in_place(&mut imgbuf);
-    imgbuf.save("output.png").unwrap();
+    imgbuf.save("output_dda_reverse.png").unwrap();
 }
